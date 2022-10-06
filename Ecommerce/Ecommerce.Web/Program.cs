06ded16dc58989fd6faf4602c88552e0ce36ed69
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2022-10-06 15:12:46 +0545

    Added Frontend Authorization

--- Ecommerce/Ecommerce.Web/Program.cs
+++ Ecommerce/Ecommerce.Web/Program.cs
@@ -3,6 +3,8 @@ using Ecommerce.Services.UI.AuthServicees;
 using Ecommerce.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+using BlazorEcommerce.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,4 +14,7 @@ builder.Services.AddScoped
     (sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetSection("BaseURL").Value) });
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 await builder.Build().RunAsync();
